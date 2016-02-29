26bab94cb142599cc1dff947d8d26fbab7d38bb5
Rebecca Dacko-Brink <Rebecca Dacko-Brink>
2016-02-29 06:21:55 -0800

    placing final aesthetic details

---
+++ src/app/report/ReportFiledCtrl.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('red')
+    .controller('ReportCtrl', ReportCtrl);
+
+  /** @ngInject */
+  function ReportCtrl($scope, $rootScope, $filter, $cookies, $state, $http) {
+    var ALIENS_GET_URL = 'https://red-wdp-api.herokuapp.com/api/mars/aliens';
+    var REPORT_POST_URL = 'https://red-wdp-api.herokuapp.com/api/mars/encounters';
+
+      // this.online = true;
+
+    $scope.encounter = {
+      // colonist_id: $cookies.getObject('session-colonist').id,
+      date: $filter('date')(Date.now(), 'yyyy-MM-dd')
+    };
+
+    $http({
+      method: 'GET',
+      url: ALIENS_GET_URL
+
+    }).then(function(response){
+      $scope.aliens = response.data.aliens;
+    }, function (error){
+    });
+
+    $scope.submit = function(event){
+        event.preventDefault();
+
+
+        $http({
+          method: 'POST',
+          url: REPORT_POST_URL,
+          data: {'encounter': $scope.encounter}
+
+        }).then(function(response){
+          $state.go('encounters');
+
+          console.log(response);
+        }, function(error){
+          console.log(error);
+        });
+    };
+  }
+
+})();
